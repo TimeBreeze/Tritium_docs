06d1ce30102d8315b30a3650056aa0f8dba7f5bb
“Oldmemorie” <3293811490@qq.com>
2024-01-26 06:15:34 +0800

    内容配置更新

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -90,18 +90,7 @@ export default defineConfig({
    
   },
 
-  ignoreDeadLinks: [
-    // ignore exact url "/playground"
-    '/playground',
-    // ignore all localhost links
-    /^https?:\/\/localhost/,
-    // ignore all links include "/repl/""
-    /\/repl\//,
-    // custom function, ignore all links include "ignore"
-    (url) => {
-      return url.toLowerCase().includes('ignore')
-    },
-  ],
+  
   vite: {
     server: {
       host: true,
